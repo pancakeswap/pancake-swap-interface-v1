889e540a2ce74073f2e8c249554213ed406ae8bd
RichardPK <r.phillips.kerr@gmail.com>
2020-10-02 17:18:34 +0100

    Translation-changing text added

--- src/utils/translateString.ts
+++ src/utils/translateString.ts
@@ -1,4 +1,17 @@
+import { useContext } from 'react'
+import { TranslationsContext } from '../hooks/TranslationsContext'
+
+const getTranslation = (translations: Array<any>, id: number) => {
+  const foundTranslation = translations.find(translation => {
+    return translation.data.stringId === id
+  })
+  return foundTranslation.data.text
+}
+
 export const TranslateString = (id: number, fallback: string) => {
-  console.log(id)
+  const { translations } = useContext(TranslationsContext)
+  if (translations.length > 0) {
+    return getTranslation(translations, id)
+  }
   return fallback
 }
