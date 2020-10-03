df8705d9ce7e479a20cfd5daf9d10ea85b6199d4
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-10-03 16:30:09 +0100

    Added all languages

--- src/constants/localisation/languageCodes.ts
+++ src/constants/localisation/languageCodes.ts
@@ -1,2 +1,61 @@
 export const DE = 'de'
 export const EN = 'en'
+export const AR = 'ar'
+export const CA = 'ca'
+export const ZHCN = 'zh-CN'
+export const ZHTW = 'zh-TW'
+export const CS = 'cs'
+export const DA = 'da'
+export const NL = 'nl'
+export const FIL = 'fil'
+export const FI = 'fi'
+export const FR = 'fr'
+export const EL = 'el'
+export const HI = 'hi'
+export const HU = 'hu'
+export const ID = 'id'
+export const IT = 'it'
+export const JA = 'ja'
+export const KO = 'ko'
+export const PTBR = 'pt-BR'
+export const RO = 'ro'
+export const RU = 'ru'
+export const SR = 'sr'
+export const ESES = 'es-ES'
+export const SVSE = 'sv-SE'
+export const TA = 'ta'
+export const TR = 'tr'
+export const UK = 'uk'
+export const VI = 'vi'
+
+export const allLanguages = [
+  DE,
+  EN,
+  AR,
+  CA,
+  ZHCN,
+  ZHTW,
+  CS,
+  DA,
+  NL,
+  FIL,
+  FI,
+  FR,
+  EL,
+  HI,
+  HU,
+  ID,
+  IT,
+  JA,
+  KO,
+  PTBR,
+  RO,
+  RU,
+  SR,
+  ESES,
+  SVSE,
+  TA,
+  TR,
+  UK,
+  VI
+]
