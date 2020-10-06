f582b7250c5c3a59df4301aa1addabb70980f989
RichardPK <r.phillips.kerr@gmail.com>
2020-10-06 22:03:43 +0100

    Prevent english pop-in

--- src/utils/translateTextHelpers.ts
+++ src/utils/translateTextHelpers.ts
@@ -1,17 +1,37 @@
 import { useContext } from 'react'
 import { TranslationsContext } from '../hooks/TranslationsContext'
 
-export const getTranslation = (translations: Array<any>, translationId: number) => {
+export const getTranslation = (translations: Array<any>, translationId: number, fallback: string) => {
   const foundTranslation = translations.find(translation => {
     return translation.data.stringId === translationId
   })
-  return foundTranslation && foundTranslation.data.text
+  if (foundTranslation) {
+    return foundTranslation.data.text
+  } else {
+    return fallback
+  }
 }
 
 export const TranslateString = (translationId: number, fallback: string) => {
   const { translations } = useContext(TranslationsContext)
   if (translations.length > 0) {
-    return getTranslation(translations, translationId)
+    return getTranslation(translations, translationId, fallback)
   }
-  return fallback
 }
+
+// export const getTranslation = (translations: Array<any>, translationId: number) => {
+//   const foundTranslation = translations.find(translation => {
+//     return translation.data.stringId === translationId
+//   })
+//   if (foundTranslation) {
+//     return foundTranslation.data.text
+//   }
+// }
+
+// export const TranslateString = (translationId: number, fallback: string) => {
+//   const { translations } = useContext(TranslationsContext)
+//   if (translations.length > 0) {
+//     return getTranslation(translations, translationId)
+//   }
+//   return fallback
+// }
