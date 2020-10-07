16f90eaf4045ee9f953dca7b9e69c965ddfd869f
RichardPK <r.phillips.kerr@gmail.com>
2020-10-07 19:31:09 +0100

    Remove commented translation code

--- src/utils/translateTextHelpers.ts
+++ src/utils/translateTextHelpers.ts
@@ -18,20 +18,3 @@ export const TranslateString = (translationId: number, fallback: string) => {
     return getTranslation(translations, translationId, fallback)
   }
 }
-
-// export const getTranslation = (translations: Array<any>, translationId: number) => {
-//   const foundTranslation = translations.find(translation => {
-//     return translation.data.stringId === translationId
-//   })
-//   if (foundTranslation) {
-//     return foundTranslation.data.text
-//   }
-// }
-
-// export const TranslateString = (translationId: number, fallback: string) => {
-//   const { translations } = useContext(TranslationsContext)
-//   if (translations.length > 0) {
-//     return getTranslation(translations, translationId)
-//   }
-//   return fallback
-// }
