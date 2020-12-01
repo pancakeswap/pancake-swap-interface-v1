ae6543b27461cf93804b977ffbeb763f24038ec3
emmaodia <odia.emma@gmail.com>
2020-12-01 17:35:53 +0100

    CHORE: Import Nav from UI Kit Library

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,123 +1,31 @@
-import React, { useRef } from 'react'
-import { Info, BookOpen, Code, PieChart, MessageCircle } from 'react-feather'
-import styled from 'styled-components'
-import { ReactComponent as MenuIcon } from '../../assets/images/menu.svg'
-import { useOnClickOutside } from '../../hooks/useOnClickOutside'
-import useToggle from '../../hooks/useToggle'
-import { ExternalLink } from '../Shared'
-import TranslatedText from '../TranslatedText'
+import React, { useContext } from 'react'
+import { Nav } from '@pancakeswap-libs/uikit'
+import useTheme from '../../hooks/useTheme'
+import { useWallet } from 'use-wallet'
+import { LanguageContext } from '../../contexts/Localisation/languageContext'
 
-const StyledMenuIcon = styled(MenuIcon)`
-  path {
-    stroke: ${({ theme }) => theme.colors.text};
-  }
-`
+import { allLanguages } from '../../constants/localisation/languageCodes'
+import { useCakePriceUSD } from '../../hooks/useTokenBalance'
 
-const StyledMenuButton = styled.button`
-  width: 100%;
-  height: 100%;
-  border: none;
-  background-color: transparent;
-  margin: 0;
-  padding: 0;
-  height: 35px;
-  background-color: ${({ theme }) => theme.colors.tertiary};
-
-  padding: 0.15rem 0.5rem;
-  border-radius: 0.5rem;
-
-  :hover,
-  :focus {
-    cursor: pointer;
-    outline: none;
-    background-color: ${({ theme }) => theme.colors.backgroundDisabled};
-  }
-
-  svg {
-    margin-top: 2px;
-  }
-`
-
-const StyledMenu = styled.div`
-  margin-left: 0.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  border: none;
-  text-align: left;
-`
-
-const MenuFlyout = styled.span`
-  min-width: 8.125rem;
-  background-color: ${({ theme }) => theme.colors.tertiary};
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
-    0px 24px 32px rgba(0, 0, 0, 0.01);
-  border-radius: 0.5rem;
-  padding: 0.5rem;
-  display: flex;
-  flex-direction: column;
-  font-size: 1rem;
-  position: absolute;
-  top: 3rem;
-  right: 0rem;
-  z-index: 100;
-`
-
-const MenuItem = styled(ExternalLink)`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  padding: 0.5rem 0.5rem;
-  color: ${({ theme }) => theme.colors.textSubtle};
-  :hover {
-    color: ${({ theme }) => theme.colors.text};
-    cursor: pointer;
-    text-decoration: none;
-  }
-  > svg {
-    margin-right: 8px;
-  }
-`
-
-const CODE_LINK = 'https://github.com/pancakeswap'
-
-export default function Menu() {
-  const node = useRef<HTMLDivElement>()
-  const [open, toggle] = useToggle(false)
-
-  useOnClickOutside(node, open ? toggle : undefined)
+const Menu = () => {
+  const { account, connect, reset } = useWallet()
+  const { isDark, toggleTheme } = useTheme()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const cakePriceUsd = useCakePriceUSD()
 
   return (
-    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
-    <StyledMenu ref={node as any}>
-      <StyledMenuButton onClick={toggle}>
-        <StyledMenuIcon />
-      </StyledMenuButton>
-      {open && (
-        <MenuFlyout>
-          <MenuItem id="link" href="/">
-            <Info size={14} />
-            About
-          </MenuItem>
-          <MenuItem id="link" href="https://docs.pancakeswap.finance">
-            <BookOpen size={14} />
-            <TranslatedText translationId={10}>Docs</TranslatedText>
-          </MenuItem>
-          <MenuItem id="link" href={CODE_LINK}>
-            <Code size={14} />
-            Code
-          </MenuItem>
-          <MenuItem id="link" href="https://t.me/PancakeSwap">
-            <MessageCircle size={14} />
-            <TranslatedText translationId={34}>Telegram</TranslatedText>
-          </MenuItem>
-          <MenuItem id="link" href="https://pancakeswap.info">
-            <PieChart size={14} />
-            Analytics
-          </MenuItem>
-        </MenuFlyout>
-      )}
-    </StyledMenu>
+    <Nav
+      account={account!}
+      login={connect}
+      logout={reset}
+      isDark={isDark!}
+      toggleTheme={toggleTheme}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
+      currentLang={setSelectedLanguage && selectedLanguage.code}
+      cakePriceUsd={cakePriceUsd}
+    />
   )
 }
+
+export default Menu
