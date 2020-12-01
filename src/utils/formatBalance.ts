de03fd244d3c0bbaacef5a5d327738919a6fa52b
emmaodia <odia.emma@gmail.com>
2020-12-01 17:43:47 +0100

    CHORE: Include formatBalance implementation in utils dir.

---
+++ src/utils/formatBalance.ts
@@ -0,0 +1,10 @@
+import BigNumber from 'bignumber.js'
+
+export const getBalanceNumber = (balance: BigNumber, decimals = 18) => {
+  const displayBalance = balance.dividedBy(new BigNumber(10).pow(decimals))
+  return displayBalance.toNumber()
+}
+
+export const getFullDisplayBalance = (balance: BigNumber, decimals = 18) => {
+  return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed()
+}
