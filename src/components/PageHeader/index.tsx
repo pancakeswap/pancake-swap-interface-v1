60028030ecefa4f47f3c370d5fb30d2d012f7d34
hachiojidev <hachiojidev@protonmail.com>
2020-12-18 14:48:45 +0900

    refactor(ui): Nav
    - Updated styles to match UI Kit

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components'
+import { Heading, Text } from '@pancakeswap-libs/uikit'
+
+interface PageHeaderProps {
+  title: ReactNode
+  description?: ReactNode
+  children?: ReactNode
+}
+
+const StyledPageHeader = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
+  padding: 24px;
+`
+
+const TitleSection = styled.div`
+  align-items: center;
+  display: block;
+  margin-bottom: 16px;
+`
+
+const Details = styled.div`
+  flex: 1;
+`
+
+const PageHeader = ({ title, description, children }: PageHeaderProps) => {
+  return (
+    <StyledPageHeader>
+      <TitleSection>
+        <Details>
+          <Heading mb="8px">{title}</Heading>
+          {description && (
+            <Text color="textSubtle" fontSize="14px">
+              {description}
+            </Text>
+          )}
+        </Details>
+      </TitleSection>
+      {children}
+    </StyledPageHeader>
+  )
+}
+
+export default PageHeader
