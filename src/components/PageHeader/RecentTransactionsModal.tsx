a7c1f8679f3aa34d342c05fbeeb07d6f1abf8b54
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-06 14:18:32 +0900

    feat: Recent Transactions (#120)
    * feat: Recent Transactions
    
    * feat(recenttransactions): Added text for other states

---
+++ src/components/PageHeader/RecentTransactionsModal.tsx
@@ -0,0 +1,86 @@
+import React, { useMemo } from 'react'
+import { CheckmarkCircleIcon, ErrorIcon, Flex, LinkExternal, Text, Modal, Button } from '@pancakeswap-libs/uikit'
+import { useActiveWeb3React } from 'hooks'
+import { getEtherscanLink } from 'utils'
+import { isTransactionRecent, useAllTransactions } from 'state/transactions/hooks'
+import { TransactionDetails } from 'state/transactions/reducer'
+import Loader from 'components/Loader'
+
+type RecentTransactionsModalProps = {
+  onDismiss?: () => void
+}
+
+// TODO: Fix UI Kit typings
+const defaultOnDismiss = () => null
+
+const newTransactionsFirst = (a: TransactionDetails, b: TransactionDetails) => b.addedTime - a.addedTime
+
+const getRowStatus = (sortedRecentTransaction: TransactionDetails) => {
+  const { hash, receipt } = sortedRecentTransaction
+
+  if (!hash) {
+    return { icon: <Loader />, color: 'text' }
+  }
+
+  if (hash && receipt?.status === 1) {
+    return { icon: <CheckmarkCircleIcon color="success" />, color: 'success' }
+  }
+
+  return { icon: <ErrorIcon color="failure" />, color: 'failure' }
+}
+
+const RecentTransactionsModal = ({ onDismiss = defaultOnDismiss }: RecentTransactionsModalProps) => {
+  const { account, chainId } = useActiveWeb3React()
+  const allTransactions = useAllTransactions()
+
+  // Logic taken from Web3Status/index.tsx line 175
+  const sortedRecentTransactions = useMemo(() => {
+    const txs = Object.values(allTransactions)
+    return txs.filter(isTransactionRecent).sort(newTransactionsFirst)
+  }, [allTransactions])
+  console.log(sortedRecentTransactions)
+
+  return (
+    <Modal title="Recent Transactions" onDismiss={onDismiss}>
+      {!account && (
+        <Flex justifyContent="center" flexDirection="column" alignItems="center">
+          <Text mb="8px" bold>
+            Please connect your wallet to view your recent transactions
+          </Text>
+          <Button variant="tertiary" size="sm" onClick={onDismiss}>
+            Close
+          </Button>
+        </Flex>
+      )}
+      {account && chainId && sortedRecentTransactions.length === 0 && (
+        <Flex justifyContent="center" flexDirection="column" alignItems="center">
+          <Text mb="8px" bold>
+            No recent transactions
+          </Text>
+          <Button variant="tertiary" size="sm" onClick={onDismiss}>
+            Close
+          </Button>
+        </Flex>
+      )}
+      {account &&
+        chainId &&
+        sortedRecentTransactions.map(sortedRecentTransaction => {
+          const { hash, summary } = sortedRecentTransaction
+          const { icon, color } = getRowStatus(sortedRecentTransaction)
+
+          return (
+            <>
+              <Flex key={hash} alignItems="center" justifyContent="space-between" mb="4px">
+                <LinkExternal href={getEtherscanLink(chainId, hash, 'transaction')} color={color}>
+                  {summary ?? hash}
+                </LinkExternal>
+                {icon}
+              </Flex>
+            </>
+          )
+        })}
+    </Modal>
+  )
+}
+
+export default RecentTransactionsModal
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,9 +1,8 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
-import { Heading, IconButton, Text, useModal } from '@pancakeswap-libs/uikit'
-// TODO: use UI Kit
-import { Settings } from 'react-feather'
+import { Heading, IconButton, Text, Flex, useModal } from '@pancakeswap-libs/uikit'
 import SettingsModal from './SettingsModal'
+import RecentTransactionsModal from './RecentTransactionsModal'
 
 interface PageHeaderProps {
   title: ReactNode
@@ -11,14 +10,28 @@ interface PageHeaderProps {
   children?: ReactNode
 }
 
+// TODO: use UI Kit
+const CogIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M19.43 12.98C19.47 12.66 19.5 12.34 19.5 12C19.5 11.66 19.47 11.34 19.43 11.02L21.54 9.37C21.73 9.22 21.78 8.95 21.66 8.73L19.66 5.27C19.54 5.05 19.27 4.97 19.05 5.05L16.56 6.05C16.04 5.65 15.48 5.32 14.87 5.07L14.49 2.42C14.46 2.18 14.25 2 14 2H9.99996C9.74996 2 9.53996 2.18 9.50996 2.42L9.12996 5.07C8.51996 5.32 7.95996 5.66 7.43996 6.05L4.94996 5.05C4.71996 4.96 4.45996 5.05 4.33996 5.27L2.33996 8.73C2.20996 8.95 2.26996 9.22 2.45996 9.37L4.56996 11.02C4.52996 11.34 4.49996 11.67 4.49996 12C4.49996 12.33 4.52996 12.66 4.56996 12.98L2.45996 14.63C2.26996 14.78 2.21996 15.05 2.33996 15.27L4.33996 18.73C4.45996 18.95 4.72996 19.03 4.94996 18.95L7.43996 17.95C7.95996 18.35 8.51996 18.68 9.12996 18.93L9.50996 21.58C9.53996 21.82 9.74996 22 9.99996 22H14C14.25 22 14.46 21.82 14.49 21.58L14.87 18.93C15.48 18.68 16.04 18.34 16.56 17.95L19.05 18.95C19.28 19.04 19.54 18.95 19.66 18.73L21.66 15.27C21.78 15.05 21.73 14.78 21.54 14.63L19.43 12.98ZM12 15.5C10.07 15.5 8.49996 13.93 8.49996 12C8.49996 10.07 10.07 8.5 12 8.5C13.93 8.5 15.5 10.07 15.5 12C15.5 13.93 13.93 15.5 12 15.5Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+const HistoryIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M13 3C8.03 3 4 7.03 4 12H1L4.89 15.89L4.96 16.03L9 12H6C6 8.13 9.13 5 13 5C16.87 5 20 8.13 20 12C20 15.87 16.87 19 13 19C11.07 19 9.32 18.21 8.06 16.94L6.64 18.36C8.27 19.99 10.51 21 13 21C17.97 21 22 16.97 22 12C22 7.03 17.97 3 13 3ZM12 8V13L16.28 15.54L17 14.33L13.5 12.25V8H12Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
 const StyledPageHeader = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
   padding: 24px;
-`
-
-const TitleSection = styled.div`
-  align-items: center;
-  display: flex;
 `
 
 const Details = styled.div`
@@ -27,11 +40,11 @@ const Details = styled.div`
 
 const PageHeader = ({ title, description, children }: PageHeaderProps) => {
   const [onPresentSettings] = useModal(<SettingsModal />)
-  const handleClick = () => onPresentSettings()
+  const [onPresentRecentTransactions] = useModal(<RecentTransactionsModal />)
 
   return (
     <StyledPageHeader>
-      <TitleSection>
+      <Flex alignItems="center">
         <Details>
           <Heading mb="8px">{title}</Heading>
           {description && (
@@ -40,10 +53,13 @@ const PageHeader = ({ title, description, children }: PageHeaderProps) => {
             </Text>
           )}
         </Details>
-        <IconButton variant="text" onClick={handleClick}>
-          <Settings />
+        <IconButton variant="text" onClick={onPresentSettings} title="Settings">
+          <CogIcon />
         </IconButton>
-      </TitleSection>
+        <IconButton variant="text" onClick={onPresentRecentTransactions} title="Recent transactions">
+          <HistoryIcon />
+        </IconButton>
+      </Flex>
       {children && <Text mt="16px">{children}</Text>}
     </StyledPageHeader>
   )
