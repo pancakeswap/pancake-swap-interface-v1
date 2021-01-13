d348b6f521505d470fb60a117e2bef94fa2069e8
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-13 09:44:21 +0900

    feat: Added Cake price to menu (#137)
    * feat: Added Cake price to menu
    
    * fix: Type error

---
+++ src/hooks/useGetPriceData.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+type ApiResponse = {
+  prices: {
+    [key: string]: string
+  }
+  update_at: string
+}
+
+/**
+ * Due to Cors the api was forked and a proxy was created
+ * @see https://github.com/pancakeswap/gatsby-pancake-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
+ */
+const api = 'https://api.pancakeswap.com/api/v1/price'
+
+const useGetPriceData = () => {
+  const [data, setData] = useState<ApiResponse | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api)
+        const data: ApiResponse = await response.json()
+
+        setData(data)
+      } catch (error) {
+        console.error('Unable to fetch price data:', error)
+      }
+    }
+
+    fetchData()
+  }, [setData])
+
+  return data
+}
+
+export default useGetPriceData
