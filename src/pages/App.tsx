1c32057ccb33389598222215d8320b329e2df761
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-02-16 21:20:27 +0900

    chore: Remove unused routes from uniswap codebase

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,24 +7,13 @@ import leftPancakeSvg from 'assets/svg/left-pancake.svg'
 import rightPancakeSvg from 'assets/svg/right-pancake.svg'
 import groupPancakeSvg from 'assets/svg/group-pancake.svg'
 import Popups from '../components/Popups'
-
 import Web3ReactManager from '../components/Web3ReactManager'
 import AddLiquidity from './AddLiquidity'
-import {
-  RedirectDuplicateTokenIds,
-  RedirectOldAddLiquidityPathStructure,
-  RedirectToAddLiquidity,
-} from './AddLiquidity/redirects'
-import MigrateV1 from './MigrateV1'
-import MigrateV1Exchange from './MigrateV1/MigrateV1Exchange'
-import RemoveV1Exchange from './MigrateV1/RemoveV1Exchange'
 import Pool from './Pool'
 import PoolFinder from './PoolFinder'
-// import Farm from './Farm'
 import RemoveLiquidity from './RemoveLiquidity'
-import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects'
 import Swap from './Swap'
-import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+import { RedirectPathToSwapOnly } from './Swap/redirects'
 import { EN, allLanguages } from '../constants/localisation/languageCodes'
 import { LanguageContext } from '../hooks/LanguageContext'
 import { TranslationsContext } from '../hooks/TranslationsContext'
@@ -136,19 +125,11 @@ export default function App() {
                   <Web3ReactManager>
                     <Switch>
                       <Route exact strict path="/swap" component={Swap} />
-                      <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-                      <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
                       <Route exact strict path="/find" component={PoolFinder} />
                       <Route exact strict path="/pool" component={Pool} />
-                      <Route exact strict path="/create" component={RedirectToAddLiquidity} />
                       <Route exact path="/add" component={AddLiquidity} />
-                      <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-                      <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-                      <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange} />
-                      <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
                       <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                      <Route exact strict path="/migrate/v1" component={MigrateV1} />
-                      <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} />
+
                       <Route component={RedirectPathToSwapOnly} />
                     </Switch>
                   </Web3ReactManager>
