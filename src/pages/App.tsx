aac5e6d81998cd9b1ba26f1faddd3e4db3d8631f
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-02-17 16:11:06 +0900

    fix: Arch and pancakes background (#188)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -36,17 +36,21 @@ const BodyWrapper = styled.div`
   overflow-x: hidden;
   z-index: 1;
   justify-content: center;
-
   background-image: url('/images/group-pancake.svg');
   background-repeat: no-repeat;
-  background-position: bottom center;
+  background-position: bottom 24px center;
   background-size: 90%;
 
+  ${({ theme }) => theme.mediaQueries.xs} {
+    background-size: auto;
+  }
+
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url('/images/arch-light.svg'), url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
+    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
+      url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
     background-repeat: no-repeat;
-    background-position: bottom, 10% center, 90% center;
-    background-size: contain, 20%, 20%;
+    background-position: center 420px, 10% 230px, 90% 230px;
+    background-size: contain, 266px, 266px;
     min-height: 90vh;
   }
 `
