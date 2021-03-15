58654c1a36752399e8f206cbc390624903c49f85
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-15 10:16:48 +0100

    fix: Translations in popup modals slippage and recent transactions

--- src/components/PageHeader/SettingsModal.tsx
+++ src/components/PageHeader/SettingsModal.tsx
@@ -4,17 +4,18 @@ import SlippageToleranceSetting from './SlippageToleranceSetting'
 import TransactionDeadlineSetting from './TransactionDeadlineSetting'
 
 type SettingsModalProps = {
-  onDismiss?: () => void
+  onDismiss?: () => void,
+  translateString: (translationId: number, fallback: string) => (string)
 }
 
 // TODO: Fix UI Kit typings
 const defaultOnDismiss = () => null
 
-const SettingsModal = ({ onDismiss = defaultOnDismiss }: SettingsModalProps) => {
+const SettingsModal = ({ onDismiss = defaultOnDismiss, translateString }: SettingsModalProps) => {
   return (
-    <Modal title="Settings" onDismiss={onDismiss}>
-      <SlippageToleranceSetting />
-      <TransactionDeadlineSetting />
+    <Modal title={translateString(1200, 'Settings')} onDismiss={onDismiss}>
+      <SlippageToleranceSetting translateString={translateString}/>
+      <TransactionDeadlineSetting translateString={translateString}/>
     </Modal>
   )
 }
