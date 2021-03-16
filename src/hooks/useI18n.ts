6da131695939fc908781fc370d26793864478aa7
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-16 14:28:12 +0100

    fix: Fix balance translation

--- src/hooks/useI18n.ts
+++ src/hooks/useI18n.ts
@@ -1,27 +1,48 @@
-import { useCallback, useContext } from 'react'
+import { useContext } from 'react'
 import { TranslationsContext } from 'hooks/TranslationsContext'
-import { getTranslation } from 'utils/translateTextHelpers'
+import { isEmpty } from 'lodash'
+
+interface ContextData {
+  [key: string]: number | string
+}
 
 const useI18n = () => {
   const { translations } = useContext(TranslationsContext)
 
-  /**
-   * As a temporary fix memoize the translation function so it can be used in an effect.
-   * It appears the TranslationsContext is always empty and is not currently used
-   * TODO: Figure out if the context is used and if not, remove it.
-   */
-  return useCallback(
-    (translationId: number, fallback: string) => {
-      if (translations[0] === 'error') {
-        return fallback
-      }
-      if (translations.length > 0) {
-        return getTranslation(translations, translationId, fallback)
-      }
+  return (translationId: number, fallback: string, data: ContextData = {}) => {
+    if (translations.length === 0) {
       return fallback
-    },
-    [translations]
-  )
+    }
+
+    const foundTranslation = translations.find((translation) => {
+      return translation.data.stringId === translationId
+    })
+
+    if (foundTranslation) {
+      const { text } = foundTranslation.data
+      const includesVariable = text.includes('%')
+
+      if (includesVariable) {
+        let interpolatedText = text
+
+        // If dynamic tags are found but no data was passed return the fallback
+        if (isEmpty(data)) {
+          return fallback
+        }
+
+        Object.keys(data).forEach((dataKey) => {
+          const templateKey = new RegExp(`%${dataKey}%`, 'g')
+          interpolatedText = interpolatedText.replace(templateKey, data[dataKey])
+        })
+
+        return interpolatedText
+      }
+
+      return text
+    }
+
+    return fallback
+  }
 }
 
 export default useI18n
