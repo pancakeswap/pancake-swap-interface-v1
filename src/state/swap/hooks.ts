28549cb3ea0ce134446e568253f1ba536b8af424
Mehmet Yıldırım <2213635+memoyil@users.noreply.github.com>
2021-03-16 13:46:14 +0100

    fix: Fix Selecting BNB again after change to something else (#283)

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -33,7 +33,7 @@ export function useSwapActionHandlers(): {
       dispatch(
         selectCurrency({
           field,
-          currencyId: currency instanceof Token ? currency.address : currency === ETHER ? 'ETH' : '',
+          currencyId: currency instanceof Token ? currency.address : currency === ETHER ? 'BNB' : '',
         })
       )
     },
