0f486284060fe5f2d3ddf873f62238d049ff6d36
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-22 12:40:59 +0100

    fix: Add bignumber library, add dependency to useEffect

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,19 +1,21 @@
 import { useEffect } from 'react'
+import BigNumber from 'bignumber.js'
 import useGetPriceData from './useGetPriceData'
 
 const useGetDocumentTitlePrice = () => {
   const priceData = useGetPriceData()
-  const cakePriceUsdNumber = priceData ? Number(priceData.prices.Cake) : Number.NaN
+  const cakePriceUsd = priceData ? new BigNumber(priceData.prices.Cake) : new BigNumber(0)
 
-  const cakePriceUsdString = Number.isNaN(cakePriceUsdNumber)
-    ? ''
-    : ` - $${cakePriceUsdNumber.toLocaleString(undefined, {
-        minimumFractionDigits: 3,
-        maximumFractionDigits: 3,
-      })}`
+  const cakePriceUsdString =
+    cakePriceUsd.isNaN() || cakePriceUsd.eq(0)
+      ? ''
+      : ` - $${cakePriceUsd.toNumber().toLocaleString(undefined, {
+          minimumFractionDigits: 3,
+          maximumFractionDigits: 3,
+        })}`
 
   useEffect(() => {
     document.title = `PancakeSwap${cakePriceUsdString}`
-  })
+  }, [cakePriceUsdString])
 }
 export default useGetDocumentTitlePrice
