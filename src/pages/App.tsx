87fcdc151e9ae4134c31fb8b93fda9e6881bdec8
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-06 17:41:23 +0200

    fix: Apply translation cache when refresh or enter

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense, useEffect, useState } from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import { Credentials, StringTranslations } from '@crowdin/crowdin-api-client'
+import { LangType } from '@pancakeswap-libs/uikit'
 import Popups from '../components/Popups'
 import Web3ReactManager from '../components/Web3ReactManager'
 import { RedirectDuplicateTokenIds, RedirectOldAddLiquidityPathStructure } from './AddLiquidity/redirects'
@@ -114,6 +115,11 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedLanguage])
 
+  const handleLanguageSelect = (langObject: LangType) => {
+    setSelectedLanguage(langObject)
+    localStorage.setItem('pancakeSwapLanguage', langObject.code)
+  }
+
   useGetDocumentTitlePrice()
 
   return (
@@ -121,7 +127,7 @@ export default function App() {
       <HashRouter>
         <AppWrapper>
           <LanguageContext.Provider
-            value={{ selectedLanguage, setSelectedLanguage, translatedLanguage, setTranslatedLanguage }}
+            value={{ selectedLanguage, setSelectedLanguage: handleLanguageSelect, translatedLanguage, setTranslatedLanguage }}
           >
             <TranslationsContext.Provider value={{ translations, setTranslations }}>
               <Menu>
