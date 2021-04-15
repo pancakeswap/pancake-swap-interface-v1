ae48243d5793d1a34739edd632a320f252740758
Wanlop Temsukkamon <golfwanlopt@gmail.com>
2021-04-15 12:07:48 +0700

    fix error network connector

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -97,7 +97,7 @@ class MiniRpcProvider implements AsyncSendable {
         reject,
         request: { method },
       } = byKey[result.id]
-      if (resolve && reject) {
+      if (reject) {
         if ('error' in result) {
           reject(new RequestError(result?.error?.message, result?.error?.code, result?.error?.data))
         } else if ('result' in result) {
