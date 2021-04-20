43b99a1fb69f8d6f33a8dc2d903820f505c64702
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-04-20 14:54:20 +0900

    fix: Text

--- src/components/SafeMoonWarningModal/index.tsx
+++ src/components/SafeMoonWarningModal/index.tsx
@@ -17,18 +17,10 @@ const WarningContainer = styled.div`
 `
 
 const StyledWarningIcon = styled(AlertTriangle)`
-  stroke: ${({ theme }) => theme.colors.failure};
+  stroke: ${({ theme }) => theme.colors.binance};
 `
 
-export default function SafeMoonWarningModal({
-  isOpen,
-  transactionType,
-  onConfirm,
-}: {
-  isOpen: boolean
-  transactionType: string | null
-  onConfirm: () => void
-}) {
+export default function SafeMoonWarningModal({ isOpen, onConfirm }: { isOpen: boolean; onConfirm: () => void }) {
   const [understandChecked, setUnderstandChecked] = useState(false)
   const toggleUnderstand = useCallback(() => setUnderstandChecked((uc) => !uc), [])
 
@@ -39,21 +31,17 @@ export default function SafeMoonWarningModal({
         <AutoColumn gap="lg">
           <AutoRow gap="6px">
             <StyledWarningIcon />
-            <Text color="secondary">Warning</Text>
+            <Text>Notice for trading SafeMoon</Text>
           </AutoRow>
-          {transactionType !== '' && (
-            <>
-              <Text color="secondary">
-                Please be careful when <strong>{transactionType}</strong> SafeMoon.
-              </Text>
-              <Text color="secondary">
-                To trade SAFEMOON, you must click on the settings icon and set your slippage tolerance to 12%+
-              </Text>
-              <Text color="secondary">This is because SafeMoon taxes a 10% fee on each transaction.</Text>
-              <Text color="secondary">- 5% fee = redistributed to all existing holders</Text>
-              <Text color="secondary">- 5% fee = used to add liquidity</Text>
-            </>
-          )}
+          <>
+            <Text color="secondary">
+              To trade SAFEMOON, you must click on the settings icon and{' '}
+              <strong>set your slippage tolerance to 12%+</strong>
+            </Text>
+            <Text color="secondary">This is because SafeMoon taxes a 10% fee on each transaction.</Text>
+            <Text color="secondary">• 5% fee = redistributed to all existing holders</Text>
+            <Text color="secondary">• 5% fee = used to add liquidity</Text>
+          </>
           <RowBetween>
             <div>
               <label htmlFor="understand-checkbox" style={{ cursor: 'pointer', userSelect: 'none' }}>
