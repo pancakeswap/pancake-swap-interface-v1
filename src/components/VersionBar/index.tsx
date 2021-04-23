3fdef9b729b6cccbf6fc7cf0be28d83e47b9c60c
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-04-23 13:36:45 +0100

    feat(version-bar): Break out of Menu and restyle accordingly for fixed position (#387)
    * feat(version-bar): Break out of Menu and restyle accordingly for fixed position
    
    * feat(version-bar): Remove unnecessary calc

--- src/components/VersionBar/index.tsx
+++ src/components/VersionBar/index.tsx
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 import { Text, ButtonMenu, ButtonMenuItem, Button, HelpIcon, Link } from '@pancakeswap-libs/uikit'
 
 const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
   background-color: ${({ theme }) => theme.colors.tertiary};
   padding: 16px;
+  ${({ theme }) => theme.mediaQueries.lg} {
+    padding-left: 240px;
+  }
 `
 
 const VersionBar = () => {
